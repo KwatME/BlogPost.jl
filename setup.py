a411aa0cebcd7a2453ba1ada07c9e5e4d062f45b
KwatME <kwat.me@icloud.com>
2021-07-21 20:02:53 -0600

    setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 na = "md_post"
 
 setup(
     name=na,
-    version="0.3.0",
+    version="0.4.0",
     url="https://github.com/KwatME/md_post",
     python_requires=">=3.6.0",
     install_requires=["click", "pyyaml"],
-    packages=[na],
+    packages=find_packages(),
     entry_points={
         "console_scripts": ["{0}={0}.{1}:{1}".format(na, "cli")],
     },
